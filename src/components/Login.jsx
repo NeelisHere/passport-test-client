417feb1b736c8154630d3af2fbc9b20fb205ae07
Neelabh Paul <npaul_be19@thapar.edu>
2023-12-18 15:50:51 +0530

    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,87 @@
-import { Button, Center, Input, VStack } from '@chakra-ui/react'
-import React from 'react'
+// import { Button, Center, Input, Text, VStack, IconButton, FormControl } from '@chakra-ui/react'
+// import { useForm } from 'react-hook-form';
+// import { FaGoogle, FaGithub, FaLinkedinIn } from "react-icons/fa";
+// import { Link } from 'react-router-dom';
+
+// const Login = () => {
+//     const { handleSubmit, register, formState, error } = useForm()
+//     const onSubmit = async (data) => {
+//         console.log(data)
+//     }
+
+//     return (
+//         <VStack gap={4} borderRadius={'5px'} border={'1px solid #dedede'} p={'30px'}>
+//             <Center fontSize={'2xl'} fontWeight={'semibold'} >Login</Center>
+//             <form onSubmit={handleSubmit(onSubmit)}>
+//                 <VStack gap={3}>
+//                     <Input
+//                         type="text"
+//                         placeholder='Enter username...'
+//                         name='username'
+//                         required
+//                         {...register('username')}
+//                     />
+//                     <Input
+//                         type="password"
+//                         placeholder='Enter password...'
+//                         name='password'
+//                         required
+//                         {...register('password')}
+//                     />
+//                     <Button
+//                         w={'100%'}
+//                         type='submit'
+//                         isLoading={formState.isSubmitting}
+//                     >
+//                         Login
+//                     </Button>
+//                 </VStack>
+//             </form>
+
+//             <Text fontSize={'small'}>
+//                 Don't have an account? <Link to={'/auth/register'} >Register</Link>
+//             </Text>
+//             <Center gap={2}>
+//                 <IconButton
+//                     isRound={true}
+//                     variant={'outline'}
+//                     icon={<FaGoogle />}
+//                 />
+//                 <IconButton
+//                     isRound={true}
+//                     variant={'outline'}
+//                     icon={<FaGithub />}
+//                 />
+//                 <IconButton
+//                     isRound={true}
+//                     variant={'outline'}
+//                     icon={<FaLinkedinIn />}
+//                 />
+//             </Center>
+//         </VStack>
+//     )
+// }
+
+// export default Login
+
+
+
+import { Button, Input } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+    const { handleSubmit, register } = useForm()
+    const onSubmit = (data) => {
+        console.log(data)
+    }
     return (
-        <VStack borderRadius={'5px'} border={'1px solid #dedede'} p={'30px'}>
-            <Center fontSize={'2xl'} fontWeight={'semibold'} >Login</Center>
-            <VStack my={'20px'}>
-                <Input 
-                    variant={'filled'} 
-                    placeholder='Enter username...' 
-                />
-                <Input 
-                    variant={'filled'} 
-                    placeholder='Enter password...' 
-                />
-            </VStack>
-            <Button w={'100%'} colorScheme='orange'>Login</Button>
-        </VStack>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h1>Login</h1>
+            <Input type="text" { ...register('name') } />
+            <Button type='submit'>Login</Button>
+            <Link to={'/auth/register'}>Register</Link>
+        </form>
     )
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
+// import { FaGoogle, FaGithub, FaLinkedinIn } from "react-icons/fa";
+// import { Link } from 'react-router-dom';
+// import { Button, Center, IconButton, Input, Text, VStack } from '@chakra-ui/react'
+
+// const Register = () => {
+//     return (
+//         <VStack gap={4} borderRadius={'5px'} border={'1px solid #dedede'} p={'30px'}>
+//             <Center fontSize={'2xl'} fontWeight={'semibold'} >Register</Center>
+//             <VStack >
+//                 <Input
+//                     type="text"
+//                     placeholder='Enter username...'
+//                 />
+//                 <Input
+//                     type="email"
+//                     placeholder='Enter username...'
+//                 />
+//                 <Input
+//                     type="password" 
+//                     placeholder='Enter password...'
+//                 />
+//             </VStack>
+//             <Text fontSize={'small'}>
+//                 Already have an account? <Link to={'/auth/login'} >Login</Link>
+//             </Text>
+//             <Button
+//                 w={'100%'}
+//             >
+//                 Register
+//             </Button>
+
+//             <Center gap={2}>
+//                 <IconButton 
+//                     isRound={true}
+//                     variant={'outline'}
+//                     icon={<FaGoogle />}
+//                 />
+//                 <IconButton 
+//                     isRound={true}
+//                     variant={'outline'}
+//                     icon={<FaGithub  />}
+//                 />
+//                 <IconButton 
+//                     isRound={true}
+//                     variant={'outline'}
+//                     icon={<FaLinkedinIn />}
+//                 />
+//             </Center>
+            
+//         </VStack>
+//     )
+// }
+
+// export default Register
+
+
+
+
+import { Button, Input } from '@chakra-ui/react'
+import { FaGoogle } from "react-icons/fa";
+// import axios from 'axios'
+// import { useForm } from 'react-hook-form'
+// import { Link } from 'react-router-dom'
 
 const Register = () => {
+    const handleClick = async () => {
+        const url = 'http://localhost:8000'
+        window.open(`${url}/auth/google`, '_self')
+    }
     return (
-        <div>
-            Register
-        </div>
+        // <form onSubmit={handleSubmit(onSubmit)}>
+        //     {/* <h1>Register</h1> */}
+        //     {/* <Input type="text" { ...register('name') } />
+        //     <Button type='submit'>Register</Button> */}
+            
+        //     {/* <Link to={'/auth/login'}>Login</Link> */}
+        // </form>
+        <Button onClick={handleClick} leftIcon={<FaGoogle/>}>Register with Google</Button>
     )
 }
 
